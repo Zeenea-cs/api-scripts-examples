763c99abd0376280be1b60708333c071b27d026a
Jérôme Mainaud <jmainaud@zeenea.com>
2024-10-09 17:54:06 +0200

    Add user group management in scim script (user.py).

--- user.py
+++ user.py
@@ -7,8 +7,6 @@ import argparse
 import sys
 from argparse import Namespace
 
-from scim2_models import User
-
 from zeenea.config import read_configuration
 from zeenea.scim import ZeeneaScimClient, ScimError, ScimNotFound, ScimTooMany, ZeeneaUser
 
@@ -20,6 +18,8 @@ def main() -> None:
             create_user(arguments)
         case 'delete':
             delete_user(arguments)
+        case 'modify':
+            modify_user(arguments)
         case _:
             print(f'Unknown action: {arguments.action}')
             sys.exit(1)
@@ -34,8 +34,39 @@ def create_user(arguments: Namespace) -> None:
         match scim_client.create_user(user):
             case ZeeneaUser() as new_user:
                 print(f"Created new user {new_user.email} ({new_user.id})")
-                if 'groups' in arguments and arguments.groups:
-                    add_user_to_groups(scim_client, new_user.id, arguments.group)
+                if 'group' in arguments and arguments.group:
+                    add_user_to_groups(scim_client, new_user, arguments.group)
+            case ScimError() as e:
+                print(e.message, file=sys.stderr)
+                sys.exit(1)
+
+
+def modify_user(arguments: Namespace) -> None:
+    given_name: str = arguments.given_name if 'given_name' in arguments else None
+    family_name: str = arguments.family_name if 'family_name' in arguments else None
+    new_groups: set[str] = set(arguments.group) if 'group' in arguments and arguments.group else None
+
+    if given_name is None and family_name is None and new_groups is None:
+        print("Nothing to modify")
+        sys.exit(0)
+
+    with open_scim_client() as scim_client:
+        match scim_client.find_user(arguments.email):
+            case ZeeneaUser() as user:
+                print(f"Found user {user.email} ({user.id})")
+                if given_name is not None or family_name is not None:
+                    modified_user = ZeeneaUser(email=user.email, id=user.id, given_name=given_name, family_name=family_name)
+                    match scim_client.modify_user(modified_user):
+                        case ZeeneaUser():
+                            print(f"Modified user {user.email} ({user.id})")
+                        case ScimError() as e:
+                            print(f"Failed to modify user {user.email} ({user.id}) {e.message}", file=sys.stderr)
+                if new_groups:
+                    old_groups: set[str] = user.groups
+                    remove_groups: list[str] = list(filter(lambda g: not g in new_groups, old_groups))
+                    add_groups: list[str] = list(filter(lambda g: not g in old_groups, new_groups))
+                    remove_user_from_groups(scim_client, user, remove_groups)
+                    add_user_to_groups(scim_client, user, add_groups)
             case ScimError() as e:
                 print(e.message, file=sys.stderr)
                 sys.exit(1)
@@ -45,7 +76,7 @@ def delete_user(arguments: Namespace) -> None:
     with open_scim_client() as scim_client:
         email = arguments.email
         match scim_client.delete_user(email):
-            case User() as user:
+            case ZeeneaUser() as user:
                 print(f"Deleted user {email} ({user.id})")
             case ScimNotFound() | ScimTooMany() as e:
                 print(e)
@@ -54,9 +85,20 @@ def delete_user(arguments: Namespace) -> None:
                 sys.exit(1)
 
 
-def add_user_to_groups(client: ZeeneaScimClient, user_id: str, group_list: list[str]):
+def add_user_to_groups(client: ZeeneaScimClient, user: ZeeneaUser, group_list: list[str]):
     for group_name in group_list:
-        match client.group_add_user(group_name, user_id):
+        match client.group_add_user(group_name, user.id):
+            case None:
+                print(f"Added user {user.email} ({user.id}) to group {group_name}")
+            case ScimError() as e:
+                print(e, file=sys.stderr)
+
+
+def remove_user_from_groups(client: ZeeneaScimClient, user: ZeeneaUser, group_list: list[str]):
+    for group_name in group_list:
+        match client.group_remove_user(group_name, user.id):
+            case None:
+                print(f"Removed user {user.email} ({user.id}) from group {group_name}")
             case ScimError() as e:
                 print(e, file=sys.stderr)
 
@@ -80,6 +122,14 @@ def parse_arguments() -> argparse.Namespace:
     delete_parser = subparsers.add_parser('delete', help="Delete an existing user")
     delete_parser.set_defaults(action='delete')
     delete_parser.add_argument('-e', '--email', required=True, help="Email address")
+
+    modify_parser = subparsers.add_parser('modify', help="Modify a user")
+    modify_parser.set_defaults(action='modify')
+    modify_parser.add_argument('-e', '--email', required=True, help="Email address")
+    modify_parser.add_argument('--given-name', help="Given name")
+    modify_parser.add_argument('--family-name', help="Family name")
+    modify_parser.add_argument('-g', '--group', action='append', help='Group to add the user to')
+
     return main_parser.parse_args()
 
 
--- zeenea/scim.py
+++ zeenea/scim.py
@@ -3,11 +3,11 @@
 
 import re
 from types import TracebackType
-from typing import Self
+from typing import Self, Union, Optional, Dict, List
 
 import httpx
 from scim2_client import SCIMClient
-from scim2_models import User, Group, Error, PatchOp, PatchOperation, SearchRequest, ListResponse, Name
+from scim2_models import User, Group, Error, PatchOp, PatchOperation, SearchRequest, ListResponse, Name, AnyResource
 
 
 class ScimError:
@@ -29,11 +29,17 @@ class ScimTooMany(ScimError):
 
 
 class ZeeneaUser:
-    def __init__(self, email: str, *, given_name: str, family_name: str, id: str | None = None) -> None:
+    def __init__(self, email: str,
+                 *,
+                 id: str | None = None,
+                 given_name: str | None = None,
+                 family_name: str | None = None,
+                 groups: set[str] | None = None) -> None:
         self.email: str = email
         self.given_name: str = given_name
         self.family_name: str = family_name
         self.id: str = id
+        self.groups = groups if groups is not None else set()
 
     @classmethod
     def from_scim(cls, scim_user: User) -> Self:
@@ -43,7 +49,12 @@ class ZeeneaUser:
         else:
             given_name = None
             family_name = None
-        return ZeeneaUser(scim_user.user_name, given_name=given_name, family_name=family_name, id=scim_user.id)
+        groups = set(map(lambda gm: gm.display, scim_user.groups)) if scim_user.groups else None
+        return ZeeneaUser(scim_user.user_name,
+                          given_name=given_name,
+                          family_name=family_name,
+                          id=scim_user.id,
+                          groups=groups)
 
     def to_scim(self):
         email: str = self.email
@@ -55,6 +66,11 @@ class ZeeneaUser:
 
 class ZeeneaScimClient:
     def __init__(self, *, tenant: str, api_secret: str):
+        """
+        Initialize a new ZeeneaScimClient instance.
+        :param tenant: Zeenea tenant name or URL.
+        :param api_secret: Zeenea API Secret.
+        """
         if not re.match('^https?://', tenant):
             url = f"https://{tenant}.zeenea.app/api/scim/v2"
         else:
@@ -79,6 +95,11 @@ class ZeeneaScimClient:
         self.http_client.__exit__(exc_type, exc_val, exc_tb)
 
     def create_user(self, user: ZeeneaUser) -> ZeeneaUser | ScimError:
+        """
+        Create a new user.
+        :param user: User to create.
+        :return: The new user (new object with the identifier).
+        """
         scim_user = user.to_scim()
         match self.scim_client.create(scim_user, raise_scim_errors=False):
             case User() as new_user:
@@ -89,7 +110,37 @@ class ZeeneaScimClient:
                 return ScimError(
                     f"Failed to create user {user.email}: unknown result ({type(unknown)}): {unknown}")
 
+    def modify_user(self, user: ZeeneaUser) -> ZeeneaUser | ScimError:
+        """
+        Modify a new user.
+        :param user: User to modify.
+        :return: The new user value.
+        """
+        scim_user = user.to_scim()
+        operations = []
+        if user.given_name is not None:
+            operations.append(PatchOperation(op=PatchOperation.Op.replace_,
+                                             path='name.givenName',
+                                             value=user.given_name))
+        if user.family_name is not None:
+            operations.append(PatchOperation(op=PatchOperation.Op.replace_,
+                                             path='name.familyName',
+                                             value=user.family_name))
+        match self.__scim_modify(scim_user, PatchOp(operations=operations), raise_scim_errors=False):
+            case User() as modified_user:
+                return ZeeneaUser.from_scim(modified_user)
+            case Error() as e:
+                return ScimError(f"Failed to modify user {user.email} ({user.id}): {e.status} {e.detail}")
+            case _ as unknown:
+                return ScimError(
+                    f"Failed to modify user {user.email} ({user.id}): unknown result ({type(unknown)}): {unknown}")
+
     def delete_user(self, email: str) -> ZeeneaUser | ScimError:
+        """
+        Delete a user with the given e-mail.
+        :param email: User e-mail.
+        :return: The former user or an error message.
+        """
         match self.find_user(email):
             case ZeeneaUser() as user:
                 match self.scim_client.delete(User, user.id, raise_scim_errors=False):
@@ -103,6 +154,11 @@ class ZeeneaScimClient:
                 return err
 
     def find_user(self, email: str) -> ZeeneaUser | ScimError:
+        """
+        Find a user with its e-mail.
+        :param email: User e-mail.
+        :return: The user or an error.
+        """
         match self.scim_client.query(User, search_request=SearchRequest(filter=f'userName eq "{email}"'),
                                      raise_scim_errors=False):
             case ListResponse() as response:
@@ -125,14 +181,48 @@ class ZeeneaScimClient:
                 return ScimError(f"Failed to find user {email}: unknown result ({type(unknown)}): {unknown}")
 
     def group_add_user(self, group_name: str, user_id: str) -> None | ScimError:
+        """
+        Add a user to a group.
+        :param group_name: Group name.
+        :param user_id: User identifier
+        :return: None or an error.
+        """
         match self.find_group_by_name(group_name):
             case Group() as group:
-                # TODO modifier le groupe
-                self.scim_client.modify(group, PatchOp({'operations': PatchOperation({'op': PatchOperation.Op.add, })}))
+                operation = PatchOperation(op=PatchOperation.Op.add, path='members', value=[{'value': user_id}])
+                match self.__scim_modify(group, PatchOp(operations=[operation]), raise_scim_errors=False):
+                    case Group() | None:
+                        return None
+                    case Error() as e:
+                        return ScimError(f"Failed to modify group {group_name}: {e.status} {e.detail}")
+                    case unknown:
+                        return ScimError(
+                            f"Failed to modify group {group_name}: unknown result ({type(unknown)}): {unknown}")
+            case ScimError() as e:
+                return e
+            case unknown:
+                return ScimError(f"Unexpected response type from find_group_by_name: {type(unknown)}")
+
+    def group_remove_user(self, group_name: str, user_id: str) -> None | ScimError:
+        """
+        Remove a user from a group.
+        :param group_name: Group name
+        :param user_id: User identifier
+        :return: None or an error.
+        """
+        match self.find_group_by_name(group_name):
+            case Group() as group:
+                operation = PatchOperation(op=PatchOperation.Op.remove, path='members', value=[{'value': user_id}])
+                self.__scim_modify(group, PatchOp(operations=[operation]))
             case ScimError() as e:
                 return e
 
     def find_group_by_name(self, name: str) -> Group | ScimError:
+        """
+        Find a group by its name.
+        :param name: Name of the group.
+        :return: The group or an error.
+        """
         match self.scim_client.query(Group, search_request=SearchRequest(filter=f'displayName eq "{name}"'),
                                      raise_scim_errors=False):
             case ListResponse() as response:
@@ -154,3 +244,99 @@ class ZeeneaScimClient:
                 return ScimError(f"Failed to find group {name}: {e.status} {e.detail}")
             case _ as unknown:
                 return ScimError(f"Failed to find group {name}: unknown result ({type(unknown)}): {unknown}")
+
+    def __scim_modify(
+        self,
+        resource: Union[AnyResource, Dict],
+        op: PatchOp,
+        check_request_payload: bool = True,
+        check_response_payload: bool = True,
+        expected_status_codes: Optional[List[int]] = SCIMClient.REPLACEMENT_RESPONSE_STATUS_CODES,
+        raise_scim_errors: bool = True,
+        **kwargs
+    ) -> Optional[Union[AnyResource, Dict]]:
+        """
+        Private Workaround. Implements the missing implementation of self.scim_client.modify.
+
+        :param resource: The new resource to modify.
+            If is a :data:`dict`, the resource type will be guessed from the schema.
+        :param check_request_payload: If :data:`False`,
+            :code:`resource` is expected to be a dict that will be passed as-is in the request.
+        :param check_response_payload: Whether to validate that the response payload is valid.
+            If set, the raw payload will be returned.
+        :param expected_status_codes: The list of expected status codes form the response.
+            If :data:`None` any status code is accepted.
+        :param raise_scim_errors: If :data:`True` and the server returned an
+            :class:`~scim2_models.Error` object, a :class:`~scim2_client.SCIMResponseErrorObject`
+            exception will be raised. If :data:`False` the error object is returned.
+        :param kwargs: Additional parameters passed to the underlying
+            HTTP request library.
+
+        :return:
+            - An :class:`~scim2_models.Error` object in case of error.
+            - The updated object as returned by the server in case of success.
+        """
+        from scim2_models import Resource, Context
+        from scim2_client import SCIMRequestError, RequestPayloadValidationError, RequestNetworkError
+        from pydantic import ValidationError
+        from httpx import RequestError
+        import sys
+
+        if not check_request_payload:
+            payload = resource
+            url = kwargs.pop("url", None)
+
+        else:
+            if isinstance(resource, Resource):
+                resource_type = resource.__class__
+
+            else:
+                resource_type = Resource.get_by_payload(self.scim_client.resource_types, resource)
+                if not resource_type:
+                    raise SCIMRequestError(
+                        "Cannot guess resource type from the payload",
+                        source=resource,
+                    )
+
+                try:
+                    resource = resource_type.model_validate(resource)
+                except ValidationError as exc:
+                    scim_exc = RequestPayloadValidationError(source=resource)
+                    if sys.version_info >= (3, 11):  # pragma: no cover
+                        scim_exc.add_note(str(exc))
+                    raise scim_exc from exc
+
+            self.scim_client.check_resource_type(resource_type)
+
+            if not resource.id:
+                raise SCIMRequestError("Resource must have an id", source=resource)
+
+            try:
+                operation = PatchOp.model_validate(op)
+            except ValidationError as exc:
+                scim_exc = RequestPayloadValidationError(source=op)
+                if sys.version_info >= (3, 11):  # pragma: no cover
+                    scim_exc.add_note(str(exc))
+                raise scim_exc from exc
+
+            payload = operation.model_dump(scim_ctx=Context.RESOURCE_REPLACEMENT_REQUEST)
+            url = kwargs.pop(
+                "url", self.scim_client.resource_endpoint(resource_type) + f"/{resource.id}"
+            )
+
+        try:
+            response = self.http_client.patch(url, json=payload, **kwargs)
+        except RequestError as exc:
+            scim_exc = RequestNetworkError(source=payload)
+            if sys.version_info >= (3, 11):  # pragma: no cover
+                scim_exc.add_note(str(exc))
+            raise scim_exc from exc
+
+        return self.scim_client.check_response(
+            response=response,
+            expected_status_codes=expected_status_codes,
+            expected_types=([resource.__class__] if check_request_payload else None),
+            check_response_payload=check_response_payload,
+            raise_scim_errors=raise_scim_errors,
+            scim_ctx=Context.RESOURCE_REPLACEMENT_RESPONSE,
+        )
